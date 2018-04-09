e57944b092d8cb08dd4667b271709c0658407301
Dave Moore <davemoore.cc@gmail.com>
2018-04-09 11:28:06 -0400

    Added integration test for an index field with multiple inner properties and a multi-field.

--- src/test/java/io/zentity/resolution/JobIT.java
+++ src/test/java/io/zentity/resolution/JobIT.java
@@ -15,7 +15,7 @@ public class JobIT extends AbstractITCase {
 
     private final Map<String, String> params = Collections.emptyMap();
 
-    private final StringEntity testJobPayload = new StringEntity("{\n" +
+    private final StringEntity TEST_PAYLOAD_JOB = new StringEntity("{\n" +
             "  \"attributes\": {\n" +
             "    \"attribute_a\": \"a_00\"\n" +
             "  },\n" +
@@ -25,7 +25,7 @@ public class JobIT extends AbstractITCase {
             "  }\n" +
             "}", ContentType.APPLICATION_JSON);
 
-    private final StringEntity testJobMaxHopsAndDocsPayload = new StringEntity("{\n" +
+    private final StringEntity TEST_PAYLOAD_JOB_MAX_HOPS_AND_DOCS = new StringEntity("{\n" +
             "  \"attributes\": {\n" +
             "    \"attribute_d\": \"d_00\"\n" +
             "  },\n" +
@@ -34,87 +34,94 @@ public class JobIT extends AbstractITCase {
             "  }\n" +
             "}", ContentType.APPLICATION_JSON);
 
-    private final StringEntity testJobDataTypesBooleanTrue = new StringEntity("{\n" +
+    private final StringEntity TEST_PAYLOAD_JOB_DATA_TYPES_BOOLEAN_TRUE = new StringEntity("{\n" +
             "  \"attributes\": { \"attribute_type_boolean\": true },\n" +
             "  \"scope\": {\n" +
             "    \"indices\": [ \".zentity_test_index_a\" ], \"resolvers\": [ \"resolver_type_boolean\" ]\n" +
             "  }\n" +
             "}", ContentType.APPLICATION_JSON);
 
-    private final StringEntity testJobDataTypesBooleanFalse = new StringEntity("{\n" +
+    private final StringEntity TEST_PAYLOAD_JOB_DATA_TYPES_BOOLEAN_FALSE = new StringEntity("{\n" +
             "  \"attributes\": { \"attribute_type_boolean\": false },\n" +
             "  \"scope\": {\n" +
             "    \"indices\": [ \".zentity_test_index_a\" ], \"resolvers\": [ \"resolver_type_boolean\" ]\n" +
             "  }\n" +
             "}", ContentType.APPLICATION_JSON);
 
-    private final StringEntity testJobDataTypesDoublePositive = new StringEntity("{\n" +
+    private final StringEntity TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_DOUBLE_POSITIVE = new StringEntity("{\n" +
             "  \"attributes\": { \"attribute_type_double\": 3.141592653589793 },\n" +
             "  \"scope\": {\n" +
             "    \"indices\": [ \".zentity_test_index_a\" ], \"resolvers\": [ \"resolver_type_double\" ]\n" +
             "  }\n" +
             "}", ContentType.APPLICATION_JSON);
 
-    private final StringEntity testJobDataTypesDoubleNegative = new StringEntity("{\n" +
+    private final StringEntity TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_DOUBLE_NEGATIVE = new StringEntity("{\n" +
             "  \"attributes\": { \"attribute_type_double\": -3.141592653589793 },\n" +
             "  \"scope\": {\n" +
             "    \"indices\": [ \".zentity_test_index_a\" ], \"resolvers\": [ \"resolver_type_double\" ]\n" +
             "  }\n" +
             "}", ContentType.APPLICATION_JSON);
 
-    private final StringEntity testJobDataTypesFloatPositive = new StringEntity("{\n" +
+    private final StringEntity TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_FLOAT_POSITIVE = new StringEntity("{\n" +
             "  \"attributes\": { \"attribute_type_float\": 1.0 },\n" +
             "  \"scope\": {\n" +
             "    \"indices\": [ \".zentity_test_index_a\" ], \"resolvers\": [ \"resolver_type_float\" ]\n" +
             "  }\n" +
             "}", ContentType.APPLICATION_JSON);
 
-    private final StringEntity testJobDataTypesFloatNegative = new StringEntity("{\n" +
+    private final StringEntity TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_FLOAT_NEGATIVE = new StringEntity("{\n" +
             "  \"attributes\": { \"attribute_type_float\": -1.0 },\n" +
             "  \"scope\": {\n" +
             "    \"indices\": [ \".zentity_test_index_a\" ], \"resolvers\": [ \"resolver_type_float\" ]\n" +
             "  }\n" +
             "}", ContentType.APPLICATION_JSON);
 
-    private final StringEntity testJobDataTypesIntegerPositive = new StringEntity("{\n" +
+    private final StringEntity TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_INTEGER_POSITIVE = new StringEntity("{\n" +
             "  \"attributes\": { \"attribute_type_integer\": 1 },\n" +
             "  \"scope\": {\n" +
             "    \"indices\": [ \".zentity_test_index_a\" ], \"resolvers\": [ \"resolver_type_integer\" ]\n" +
             "  }\n" +
             "}", ContentType.APPLICATION_JSON);
 
-    private final StringEntity testJobDataTypesIntegerNegative = new StringEntity("{\n" +
+    private final StringEntity TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_INTEGER_NEGATIVE = new StringEntity("{\n" +
             "  \"attributes\": { \"attribute_type_integer\": -1 },\n" +
             "  \"scope\": {\n" +
             "    \"indices\": [ \".zentity_test_index_a\" ], \"resolvers\": [ \"resolver_type_integer\" ]\n" +
             "  }\n" +
             "}", ContentType.APPLICATION_JSON);
 
-    private final StringEntity testJobDataTypesLongPositive = new StringEntity("{\n" +
+    private final StringEntity TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_LONG_POSITIVE = new StringEntity("{\n" +
             "  \"attributes\": { \"attribute_type_long\": 922337203685477 },\n" +
             "  \"scope\": {\n" +
             "    \"indices\": [ \".zentity_test_index_a\" ], \"resolvers\": [ \"resolver_type_long\" ]\n" +
             "  }\n" +
             "}", ContentType.APPLICATION_JSON);
 
-    private final StringEntity testJobDataTypesLongNegative = new StringEntity("{\n" +
+    private final StringEntity TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_LONG_NEGATIVE = new StringEntity("{\n" +
             "  \"attributes\": { \"attribute_type_long\": -922337203685477 },\n" +
             "  \"scope\": {\n" +
             "    \"indices\": [ \".zentity_test_index_a\" ], \"resolvers\": [ \"resolver_type_long\" ]\n" +
             "  }\n" +
             "}", ContentType.APPLICATION_JSON);
 
-    private final StringEntity testJobDataTypesStringA = new StringEntity("{\n" +
+    private final StringEntity TEST_PAYLOAD_JOB_DATA_TYPES_STRING_A = new StringEntity("{\n" +
             "  \"attributes\": { \"attribute_type_string\": \"a\" },\n" +
             "  \"scope\": {\n" +
             "    \"indices\": [ \".zentity_test_index_a\" ], \"resolvers\": [ \"resolver_type_string\" ]\n" +
             "  }\n" +
             "}", ContentType.APPLICATION_JSON);
 
-    private final StringEntity testJobDataTypesStringB = new StringEntity("{\n" +
+    private final StringEntity TEST_PAYLOAD_JOB_DATA_TYPES_STRING_B = new StringEntity("{\n" +
             "  \"attributes\": { \"attribute_type_string\": \"b\" },\n" +
             "  \"scope\": {\n" +
             "    \"indices\": [ \".zentity_test_index_a\" ], \"resolvers\": [ \"resolver_type_string\" ]\n" +
+            "  }\n" +
+            "}", ContentType.APPLICATION_JSON);
+
+    private final StringEntity TEST_PAYLOAD_JOB_OBJECT = new StringEntity("{\n" +
+            "  \"attributes\": { \"attribute_object\": \"a\" },\n" +
+            "  \"scope\": {\n" +
+            "    \"indices\": [ \".zentity_test_index_a\" ], \"resolvers\": [ \"resolver_object\" ]\n" +
             "  }\n" +
             "}", ContentType.APPLICATION_JSON);
 
@@ -169,7 +176,7 @@ public class JobIT extends AbstractITCase {
         try {
             prepareTestResources();
             String endpoint = "_zentity/resolution/zentity_test_entity_a";
-            Response response = client.performRequest("POST", endpoint, params, testJobPayload);
+            Response response = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB);
             JsonNode json = mapper.readTree(response.getEntity().getContent());
             assertEquals(json.get("hits").get("total").asInt(), 6);
 
@@ -191,7 +198,7 @@ public class JobIT extends AbstractITCase {
         try {
             prepareTestResources();
             String endpoint = "_zentity/resolution/zentity_test_entity_a?max_hops=2&max_docs_per_query=2";
-            Response response = client.performRequest("POST", endpoint, params, testJobMaxHopsAndDocsPayload);
+            Response response = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB_MAX_HOPS_AND_DOCS);
             JsonNode json = mapper.readTree(response.getEntity().getContent());
             assertEquals(json.get("hits").get("total").asInt(), 20);
 
@@ -243,76 +250,99 @@ public class JobIT extends AbstractITCase {
             docsExpectedB.add("a9,0");
 
             // Boolean true
-            Response r1 = client.performRequest("POST", endpoint, params, testJobDataTypesBooleanTrue);
+            Response r1 = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB_DATA_TYPES_BOOLEAN_TRUE);
             JsonNode j1 = mapper.readTree(r1.getEntity().getContent());
             assertEquals(j1.get("hits").get("total").asInt(), 5);
             assertEquals(docsExpectedA, getActual(j1));
 
             // Boolean false
-            Response r2 = client.performRequest("POST", endpoint, params, testJobDataTypesBooleanFalse);
+            Response r2 = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB_DATA_TYPES_BOOLEAN_FALSE);
             JsonNode j2 = mapper.readTree(r2.getEntity().getContent());
             assertEquals(j2.get("hits").get("total").asInt(), 5);
             assertEquals(docsExpectedB, getActual(j2));
 
             // Double positive
-            Response r3 = client.performRequest("POST", endpoint, params, testJobDataTypesDoublePositive);
+            Response r3 = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_DOUBLE_POSITIVE);
             JsonNode j3 = mapper.readTree(r3.getEntity().getContent());
             assertEquals(j3.get("hits").get("total").asInt(), 5);
             assertEquals(docsExpectedA, getActual(j3));
 
             // Double negative
-            Response r4 = client.performRequest("POST", endpoint, params, testJobDataTypesDoubleNegative);
+            Response r4 = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_DOUBLE_NEGATIVE);
             JsonNode j4 = mapper.readTree(r4.getEntity().getContent());
             assertEquals(j4.get("hits").get("total").asInt(), 5);
             assertEquals(docsExpectedB, getActual(j4));
 
             // Float positive
-            Response r5 = client.performRequest("POST", endpoint, params, testJobDataTypesFloatPositive);
+            Response r5 = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_FLOAT_POSITIVE);
             JsonNode j5 = mapper.readTree(r5.getEntity().getContent());
             assertEquals(j5.get("hits").get("total").asInt(), 5);
             assertEquals(docsExpectedA, getActual(j5));
 
             // Float negative
-            Response r6 = client.performRequest("POST", endpoint, params, testJobDataTypesFloatNegative);
+            Response r6 = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_FLOAT_NEGATIVE);
             JsonNode j6 = mapper.readTree(r6.getEntity().getContent());
             assertEquals(j6.get("hits").get("total").asInt(), 5);
             assertEquals(docsExpectedB, getActual(j6));
 
             // Integer positive
-            Response r7 = client.performRequest("POST", endpoint, params, testJobDataTypesIntegerPositive);
+            Response r7 = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_INTEGER_POSITIVE);
             JsonNode j7 = mapper.readTree(r7.getEntity().getContent());
             assertEquals(j7.get("hits").get("total").asInt(), 5);
             assertEquals(docsExpectedA, getActual(j7));
 
             // Integer negative
-            Response r8 = client.performRequest("POST", endpoint, params, testJobDataTypesIntegerNegative);
+            Response r8 = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_INTEGER_NEGATIVE);
             JsonNode j8 = mapper.readTree(r8.getEntity().getContent());
             assertEquals(j8.get("hits").get("total").asInt(), 5);
             assertEquals(docsExpectedB, getActual(j8));
 
             // Long positive
-            Response r9 = client.performRequest("POST", endpoint, params, testJobDataTypesLongPositive);
+            Response r9 = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_LONG_POSITIVE);
             JsonNode j9 = mapper.readTree(r9.getEntity().getContent());
             assertEquals(j9.get("hits").get("total").asInt(), 5);
             assertEquals(docsExpectedA, getActual(j9));
 
             // Long negative
-            Response r10 = client.performRequest("POST", endpoint, params, testJobDataTypesLongNegative);
+            Response r10 = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB_DATA_TYPES_NUMBER_LONG_NEGATIVE);
             JsonNode j10 = mapper.readTree(r10.getEntity().getContent());
             assertEquals(j10.get("hits").get("total").asInt(), 5);
             assertEquals(docsExpectedB, getActual(j10));
 
             // String A
-            Response r11 = client.performRequest("POST", endpoint, params, testJobDataTypesStringA);
+            Response r11 = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB_DATA_TYPES_STRING_A);
             JsonNode j11 = mapper.readTree(r11.getEntity().getContent());
             assertEquals(j11.get("hits").get("total").asInt(), 5);
             assertEquals(docsExpectedA, getActual(j11));
 
             // String B
-            Response r12 = client.performRequest("POST", endpoint, params, testJobDataTypesStringB);
+            Response r12 = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB_DATA_TYPES_STRING_B);
             JsonNode j12 = mapper.readTree(r12.getEntity().getContent());
             assertEquals(j12.get("hits").get("total").asInt(), 5);
             assertEquals(docsExpectedB, getActual(j12));
+
+        } finally {
+            destroyTestResources();
+        }
+    }
+
+    public void testJobObject() throws Exception {
+        try {
+            prepareTestResources();
+            String endpoint = "_zentity/resolution/zentity_test_entity_a";
+
+            Set<String> docsExpectedA = new HashSet<>();
+            docsExpectedA.add("a0,0");
+            docsExpectedA.add("a2,0");
+            docsExpectedA.add("a4,0");
+            docsExpectedA.add("a6,0");
+            docsExpectedA.add("a8,0");
+
+            // Boolean true
+            Response r1 = client.performRequest("POST", endpoint, params, TEST_PAYLOAD_JOB_OBJECT);
+            JsonNode j1 = mapper.readTree(r1.getEntity().getContent());
+            assertEquals(j1.get("hits").get("total").asInt(), 5);
+            assertEquals(docsExpectedA, getActual(j1));
 
         } finally {
             destroyTestResources();
