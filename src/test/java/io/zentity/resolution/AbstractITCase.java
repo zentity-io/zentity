24c6ebafbf85ea5459c465c50df7c96d512debb6
Dave Moore <davemoore.cc@gmail.com>
2021-02-01 20:06:53 -0500

    Create and destroy the REST client on @BeforeClass and @AfterClass, instead of lazily, for each test class.

--- src/test/java/io/zentity/resolution/AbstractITCase.java
+++ src/test/java/io/zentity/resolution/AbstractITCase.java
@@ -17,8 +17,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import static org.junit.Assume.assumeFalse;
 
 public abstract class AbstractITCase {
@@ -30,12 +29,24 @@ public abstract class AbstractITCase {
     private static DockerComposeContainer cluster;
 
     // Client that communicates with the docker-compose cluster.
-    private RestClient client;
+    private static RestClient client;
 
     @BeforeClass
-    public static void setup() {
+    public static void setup() throws IOException {
+        createCluster();
+        createClient();
+    }
 
-        // Create and start the test cluster
+    @AfterClass
+    public static void tearDown() throws IOException {
+        destroyClient();
+        destroyCluster();
+    }
+
+    /**
+     * Create the cluster if it doesn't exist.
+     */
+    public static void createCluster() {
         Path path = Paths.get(System.getenv("BUILD_DIRECTORY"), "test-classes", "docker-compose.yml");
         cluster = new DockerComposeContainer(new File(path.toString()))
                 .withEnv("BUILD_DIRECTORY", System.getenv("BUILD_DIRECTORY"))
@@ -48,43 +59,57 @@ public abstract class AbstractITCase {
         cluster.start();
     }
 
-    @AfterClass
-    public static void tearDown() {
-        // Stop the test cluster if it was created.
-        if (cluster != null)
+    /**
+     * Destroy the cluster if it exists.
+     */
+    public static void destroyCluster() {
+        if (cluster != null) {
             cluster.stop();
+            cluster = null;
+        }
     }
 
     /**
-     * Create the client if it doesn't exist, and then return it.
+     * Create the client if it doesn't exist.
+     */
+    public static void createClient() throws IOException {
+        try {
+
+            // Create the client.
+            String host = cluster.getServiceHost(SERVICE_NAME, SERVICE_PORT);
+            Integer port = cluster.getServicePort(SERVICE_NAME, SERVICE_PORT);
+            client = RestClient.builder(new HttpHost(host, port)).build();
+
+            // Verify if the client can establish a connection to the cluster.
+            Response response = client.performRequest(new Request("GET", "/"));
+            JsonNode json = Json.MAPPER.readTree(response.getEntity().getContent());
+            assertEquals("You Know, for Search", json.get("tagline").textValue());
+
+        } catch (IOException e) {
+
+            // If we have an exception here, let's ignore the test.
+            destroyClient();
+            assumeFalse("Integration tests are skipped", e.getMessage().contains("Connection refused"));
+            fail("Something wrong is happening. REST Client seemed to raise an exception: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Destroy the client if it exists.
+     */
+    public static void destroyClient() throws IOException {
+        if (client != null) {
+            client.close();
+            client = null;
+        }
+    }
+
+    /**
+     * Return the client to be used in test cases.
      *
      * @return The client.
-     * @throws IOException
      */
-    public RestClient client() throws IOException {
-        if (this.client == null) {
-            try {
-
-                // Create a new client.
-                String host = cluster.getServiceHost(SERVICE_NAME, SERVICE_PORT);
-                Integer port = cluster.getServicePort(SERVICE_NAME, SERVICE_PORT);
-                this.client = RestClient.builder(new HttpHost(host, port)).build();
-
-                // Verify if the client can establish a connection to the cluster.
-                Response response = this.client.performRequest(new Request("GET", "/"));
-                JsonNode json = Json.MAPPER.readTree(response.getEntity().getContent());
-                assertTrue(json.get("tagline").textValue().equals("You Know, for Search"));
-            } catch (IOException e) {
-
-                // If we have an exception here, let's ignore the test
-                assumeFalse("Integration tests are skipped", e.getMessage().contains("Connection refused"));
-                fail("Something wrong is happening. REST Client seemed to raise an exception: " + e.getMessage());
-                if (client != null) {
-                    client.close();
-                    client = null;
-                }
-            }
-        }
+    public RestClient client() {
         return client;
     }
 }
