b2359441988d286485ccfcb56deb8c87f96028a8
Dave Moore <davemoore.cc@gmail.com>
2024-06-08 11:09:12 -0400

    Moved ZidTest to respective folder in test suites

--- src/test/java/io/zentity/model/ZidTest.java
+++ src/test/java/io/zentity/model/ZidTest.java
@@ -15,10 +15,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.zentity.common;
+package io.zentity.model;
 
-import io.zentity.model.ValidationException;
-import io.zentity.model.Zid;
 import org.junit.Assert;
 import org.junit.Test;
 
